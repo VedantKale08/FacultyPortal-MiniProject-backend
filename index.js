453f44034c2ccf43317774e2fcc419d256d58281
VedantKale08 <fscocomputer@gmail.com>
2024-05-16 19:16:52 +0530

    small changes

--- index.js
+++ index.js
@@ -9,15 +9,14 @@ const LectureHistory = require("./models/LectureHistory");
 
 const app = express();
 
+
 app.use(bodyParser.json());
 app.use(cors())
 dotenv.config();
 
+
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
